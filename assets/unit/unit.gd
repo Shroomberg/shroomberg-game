0bc2f82caefeba72bd179d026d8c6ad56c6281bd
random <randomdima@gmail.com>
2025-04-06 00:56:39 +0200

    refined state machine

--- assets/unit/unit.gd
+++ assets/unit/unit.gd
@@ -1,8 +1,7 @@
 class_name Unit extends Node2D
 
-signal unit_died();
-
-enum UnitAction { Idle, Moving, Attacking }
+enum UnitState { Idle, Moving, PreparingAction, ExecutingAction, Dead }
+enum UnitEvent { ActionPrepared, ActionExecuted, TargetDetected, UnitDied }
 
 const WALK_SPEED = 200.0
 
@@ -14,69 +13,81 @@ const WALK_SPEED = 200.0
 
 var hp: float = 10
 
-var action: UnitAction = UnitAction.Idle
+var state: UnitState = UnitState.Idle
 var target: Unit = null
-
-var action_handlers = {
-	UnitAction.Idle: process_idle,
-	UnitAction.Moving: process_moving,
-	UnitAction.Attacking: process_attacking
-}
-
-func _physics_process(delta: float) -> void:
-	action_handlers[action].call(delta)	
 
 func _ready():
 	hp = max_hp
 	$VisionArea/Shape.scale = Vector2(vision_range, vision_range)
-	set_action(UnitAction.Moving)
+	set_state(UnitState.Idle)
 	$Sprite2D.scale = Vector2(direction, 1)
 	
-func set_action(new_action: UnitAction):
-	action = new_action
-	print(UnitAction.keys()[action])
-	$Animation.play(UnitAction.keys()[action])
-	
-func process_idle(delta: float):
-	pass
-	
-func process_moving(delta: float):		
-	position.x += direction * WALK_SPEED * delta;		
-	
-func process_attacking(delta: float):
-	target.take_damage(damage)
-	if target.is_dead():
-		scan_for_targets()
+func _physics_process(delta: float) -> void:
+	match state:
+		UnitState.Moving: 
+			position.x += direction * WALK_SPEED * delta;	
 
-func is_dead():
-	return hp <= 0 or is_queued_for_deletion() 
+func set_state(new_state: UnitState):	
+	state = new_state
+	print("%s: %s " % [self.get_instance_id(), UnitState.keys()[new_state]])
+	$Animation.play(UnitState.keys()[new_state])
+	$HealthBar.visible = new_state != UnitState.Dead
 
 func take_damage(amount: int):
-	hp -= amount
-	$HealthBar.value = hp/max_hp
-	if hp <= 0:
-		kill()	
-
-func kill() -> void:	
-	unit_died.emit()
-	queue_free()
+	if hp > amount:
+		hp -= amount
+		$HealthBar.value = hp/max_hp
+	else:
+		process_event(UnitEvent.UnitDied)
 
 func can_use_target(node: Node2D) -> bool:
-	return node != self and node is Unit and !node.is_dead()
+	return node != self and node is Unit and node.state != UnitState.Dead	
 
-func set_target(node: Node2D):
-	target = node
-	set_action(UnitAction.Attacking)
+func process_event(event: UnitEvent):
+	if state == UnitState.Dead:
+		return
+	
+	match event:
+		UnitEvent.TargetDetected:			
+			if !target:
+				choose_new_target()				
+		UnitEvent.ActionExecuted:					
+			if can_use_target(target):			
+				set_state(UnitState.PreparingAction)
+			else:
+				choose_new_target()
+		UnitEvent.ActionPrepared:
+			set_state(UnitState.ExecutingAction)
+			if can_use_target(target):
+				execute_action()
+		UnitEvent.UnitDied:				
+			target = null
+			hp = 0
+			set_state(UnitState.Dead)
 
-func scan_for_targets():
-	print('scanning for targets')	
-	target = null
+func execute_action():
+	target.take_damage(damage)	
+
+func choose_new_target():
+	target = scan_for_target()
+	if target:
+		set_state(UnitState.PreparingAction)
+	else:
+		set_state(UnitState.Moving)		
+
+func scan_for_target() -> Unit:
+	print("%s: scanning for targets " % [self.get_instance_id()])
 	for node in $VisionArea.get_overlapping_bodies():
 		if can_use_target(node):
-			set_target(node)
-	if !target:
-		set_action(UnitAction.Moving)
+			return node
+	return null
 
 func _on_body_entered(node: Node2D):
-	if !target && can_use_target(node):
-		set_target(node)
+	process_event(UnitEvent.TargetDetected)
+
+func _on_animation_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"PreparingAction":
+			process_event(UnitEvent.ActionPrepared)
+		"ExecutingAction":
+			process_event(UnitEvent.ActionExecuted)
