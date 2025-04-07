7180667caa7c972f272d7c26b057e7599a40e92f
random <randomdima@gmail.com>
2025-04-07 17:38:04 +0200

    roots

--- assets/world/world.gd
+++ assets/world/world.gd
@@ -2,29 +2,28 @@ class_name World extends Node
 
 @export var map_size: int = 10;
 @export var min_groth_distance: int = 3;
-@export var max_root_distance: int = 5;
 @export var root_decay_rate: float = 1
 @export var root_groth_rate: float = 3
+@export var root_distance_penalty: float = 0.75
 @export var max_root_size: float = 10
+@export var grown_root_size: float = 5
 @export var root_state_count: int = 4
-
-var closest_root_mushroom: Dictionary[int, Mushroom] = {}
-var closest_borrowed_mushroom: Dictionary[int, Mushroom] = {}
 
 var mushrooms: Dictionary[int, Array]
 var roots: Dictionary[int, float]
-var root_distance_to_mushroom: Dictionary[int, int]
+var root_owners: Dictionary[int, Mushroom]
 func _ready():
 	for q in range(-map_size, map_size):
 		mushrooms[q] = []
 		roots[q] = 0
+		root_owners[q] = null
 	$Camera2D.slideToPosition($CameraStartPosition.global_position, 1.5)
 
 func get_root_data(position: int) -> int:
 	return $Roots.get_cell_source_id(Vector2i(position, 0))
 	
 func is_grown_root(position: int):
-	return get_root_data(position) == max_root_size
+	return roots[position] > grown_root_size
 
 func get_cell_position(postion: int) -> float:
 	return $Terrain.map_to_local(Vector2i(postion, 0)).x
@@ -45,53 +44,40 @@ func rebuild_mushrooms():
 		mushroom.world = self
 		mushroom.cell_id = $Terrain.local_to_map(mushroom.position).x
 		mushrooms[mushroom.cell_id].push_back(mushroom)
-		
-func rebuild_root_distance():
-	for q in range(-map_size, map_size):
-		root_distance_to_mushroom[q] = -1
 
 func apply_root_tiles():
 	for cell_id in range(-map_size, map_size):
 		var tile_id = round(roots[cell_id] * root_state_count / max_root_size) - 1;
 		$Roots.set_cell(Vector2i(cell_id, 0), tile_id, Vector2i.ZERO, 0)	
-
-func natural_decay(delta: float):
-	for cell_id in range(-map_size, map_size):
-		roots[cell_id] = max(0, roots[cell_id] - delta * root_decay_rate)		
 		
 func natual_groth(delta: float):
+	var allowed_sizes: Dictionary[int, float] 	
 	for cell_id in range(-map_size, map_size): 
 		var mushroom = get_borrowed_mushroom(cell_id)
 		if mushroom:
-			roots[cell_id] = min(max_root_size, roots[cell_id] + delta * root_groth_rate)
+			allowed_sizes[cell_id] = max_root_size
+			root_owners[cell_id] = mushroom
+		else:			
+			var left = roots.get(cell_id - 1, 0)
+			var right = roots.get(cell_id + 1, 0)
+			allowed_sizes[cell_id] = floor(max(left, right) * root_distance_penalty)
+			if allowed_sizes[cell_id] == 0:
+				root_owners[cell_id] = null
+			elif left > right:
+				root_owners[cell_id] = root_owners[cell_id - 1]
+			else:
+				root_owners[cell_id] = root_owners[cell_id + 1]		
+	
+	for cell_id in range(-map_size, map_size): 		
+		if roots[cell_id] > allowed_sizes[cell_id]:
+			roots[cell_id] = max(allowed_sizes[cell_id], roots[cell_id] - delta * root_decay_rate)	
+		else:
+			roots[cell_id] = min(allowed_sizes[cell_id], roots[cell_id] + delta * root_groth_rate)
 
 func _physics_process(delta: float):
-	rebuild_mushrooms()	
-	natural_decay(delta)
+	rebuild_mushrooms()
 	natual_groth(delta)
 	apply_root_tiles()
-
-#func rebuild_closest_root_mushrooms():
-	#closest_root_mushroom = {}	
-	#for d in range(0, max_root_distance):
-		#for q in mushroom_info.keys():
-			#if d == 0:
-				#closest_root_mushroom[q] = mushroom_info[q]
-			#else:
-				#if is_grown_root(q + d - 1) and !closest_root_mushroom.get(q + d):
-					#closest_root_mushroom[q + d] = mushroom_info[q]
-				#if is_grown_root(q - d + 1) and !closest_root_mushroom.get(q - d):
-					#closest_root_mushroom[q - d] = mushroom_info[q]					
-					#
-	#for d in range(0, min_mushroom_distance):
-		#for q in mushroom_info.keys():
-			#if d == 0:
-				#closest_borrowed_mushroom[q] = mushroom_info[q]
-			#else:
-				#if !closest_borrowed_mushroom.get(q + d):
-					#closest_borrowed_mushroom[q + d] = mushroom_info[q]
-				#if !closest_borrowed_mushroom.get(q - d):
-					#closest_borrowed_mushroom[q - d] = mushroom_info[q]	
 	
 func msg(msg: String):
 	print("%s: %s" % [Time.get_ticks_msec(), msg])		
